639d38731a135b4e70eb3c99c878fb6c3ab9c12e
yulmw <69083407+dyddn3994@users.noreply.github.com>
2023-03-26 21:34:09 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 입력
+        int n = Integer.parseInt(br.readLine());
+        long[] dp = new long[n+1];
+        dp[0] = 0; dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        System.out.println(dp[n]);
+
+    }
+}
