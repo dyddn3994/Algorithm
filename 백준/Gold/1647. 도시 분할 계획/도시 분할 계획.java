ca0b24ca468e405d378504a3b8d781bd6a9bcda8
yulmw <69083407+dyddn3994@users.noreply.github.com>
2023-03-29 15:40:13 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1432 ms, Memory: 322328 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -1,16 +1,39 @@
 import java.io.*;
 import java.util.*;
 
+// 노드 최대 100_000, 간선 최대 1_000_000
 class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
 	static int N; // 집의 개수
 	static int M; // 길의 개수
-	
-	static List<Node>[] list; // 길 인접리스트
-	static boolean[] visited;
+
 	static int res;
 	
-	static void prim(int x)	{
+	// prim
+	static List<Node>[] list; // 길 인접리스트
+	static boolean[] visited;
+	
+	static void prim(int x) throws IOException	{
+		list = new ArrayList[N+1];
+		for (int i = 1; i <= N; i++) {
+			list[i] = new ArrayList<>();
+		}
+		
+		for (int i = 0; i < M; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			// A번 집과 B번 집을 연결하는 길의 유지비 C
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			int C = Integer.parseInt(st.nextToken());
+			
+			list[A].add(new Node(B, C));
+			list[B].add(new Node(A, C));
+		}
+		
+		visited = new boolean[N+1];
+		
 		Queue<Node> queue = new PriorityQueue<>();
 		queue.add(new Node(x, 0));
 		int maxRoad = 0; // 가장 유지비가 큰 길의 유지비, 해당 길만 끊어서 마을 구성
@@ -36,33 +59,62 @@ class Main {
 		res -= maxRoad;
 	}
 	
+	// kruskal
+	static PriorityQueue<Node2> queue;
+	static int[] parent;
+	
+	static void kruskal() throws IOException {
+		queue = new PriorityQueue<>();
+		for (int i = 0; i < M; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			int C = Integer.parseInt(st.nextToken());
+			
+			queue.add(new Node2(A, B, C));
+		}
+		
+		parent = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			parent[i] = i;
+		}
+		
+		int maxRoad = 0; // 가장 유지비가 큰 길의 유지비, 해당 길만 끊어서 마을 구성
+		
+		while(!queue.isEmpty()) {
+			Node2 node = queue.poll();
+			
+			if (findSet(node.s) == findSet(node.e)) continue;
+			union(node.s, node.e);
+			
+			res += node.cost;
+			maxRoad = Math.max(maxRoad, node.cost);
+		}
+		
+		res -= maxRoad;
+	}
+	
+	static int findSet(int x) {
+		if (parent[x] != x) parent[x] = findSet(parent[x]);
+		return parent[x];
+	}
+	
+	static void union(int x, int y) {
+		int p1 = findSet(x);
+		int p2 = findSet(y);
+		
+		parent[p1] = p2;
+	}
+	
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		list = new ArrayList[N+1];
-		for (int i = 1; i <= N; i++) {
-			list[i] = new ArrayList<>();
-		}
+//		prim(1);
 		
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			
-			// A번 집과 B번 집을 연결하는 길의 유지비 C
-			int A = Integer.parseInt(st.nextToken());
-			int B = Integer.parseInt(st.nextToken());
-			int C = Integer.parseInt(st.nextToken());
-			
-			list[A].add(new Node(B, C));
-			list[B].add(new Node(A, C));
-		}
-		
-		visited = new boolean[N+1];
-		
-		prim(1);
+		kruskal();
 		
 		System.out.println(res);
 	}
@@ -80,5 +132,22 @@ class Node implements Comparable<Node> {
 	@Override
 	public int compareTo(Node o) {
 		return this.expense - o.expense;
+	}
+}
+
+class Node2 implements Comparable<Node2> {
+	int s;
+	int e;
+	int cost;
+	
+	Node2 (int s, int e, int cost) {
+		this.s = s;
+		this.e = e;
+		this.cost = cost;
+	}
+	
+	@Override
+	public int compareTo(Node2 o) {
+		return this.cost - o.cost;
 	}
 }
