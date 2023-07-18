c6ec6165cc3911bde2bbd2fe81cc0f5cac1036b1
yulmw <69083407+dyddn3994@users.noreply.github.com>
2023-07-19 00:00:39 +0900

    [Gold III] Title: 피리 부는 사나이, Time: 444 ms, Memory: 31324 KB -BaekjoonHub

--- 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.kt
+++ 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.kt
@@ -1,44 +1,27 @@
-
 val directionWord = arrayOf('R', 'D', 'L', 'U')
 val dir = arrayOf(0 to 1, 1 to 0, 0 to -1, -1 to 0)
 
 var N = 0
 var M = 0
 
-lateinit var visited: Array<BooleanArray>
+lateinit var visited: Array<IntArray>
 lateinit var graph: Array<CharArray>
 
 var res = 0
 
-fun isWall(x: Int, y: Int): Boolean {
-    if (x >= N || x < 0 || y >= M || y < 0) return true
-    return false
-}
-
-fun findFront(x: Int, y: Int) {
-    visited[x][y] = true
-
+fun findFront(x: Int, y: Int): Int {
     val idx = directionWord.indexOf(graph[x][y])
     val nx = x + dir[idx].first
     val ny = y + dir[idx].second
 
-    if (!visited[nx][ny]) {
-        findFront(nx, ny)
-        findBack(nx, ny)
-    }
-}
-
-fun findBack(x: Int, y: Int) {
-    visited[x][y] = true
-
-    dir.forEachIndexed { idx, (px, py) ->
-        val nx = x + px
-        val ny = y + py
-
-        if (!isWall(nx, ny) && !visited[nx][ny]) {
-            if (graph[nx][ny] == directionWord[(idx + 2) % 4]) findBack(nx, ny)
+    visited[x][y] =
+        if (visited[nx][ny] == 0) {
+            visited[nx][ny] = visited[x][y]
+            findFront(nx, ny)
         }
-    }
+        else visited[nx][ny]
+
+    return visited[x][y]
 }
 
 fun main() = with(System.`in`.bufferedReader()) {
@@ -55,14 +38,15 @@ fun main() = with(System.`in`.bufferedReader()) {
         }
     }
 
-    visited = Array(N) { BooleanArray(M) }
+    visited = Array(N) { IntArray(M) }
 
     repeat(N) { i ->
         repeat(M) { j ->
-            if (!visited[i][j]) {
-                findBack(i, j)
+            if (visited[i][j] == 0) {
+                visited[i][j] = res + 1
                 findFront(i, j)
-                res++
+
+                if (visited[i][j] == res + 1) res++
             }
         }
     }
