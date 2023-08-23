26e5a429ee67398deaa5750b39cf6e0ec82b8e38
yulmw <69083407+dyddn3994@users.noreply.github.com>
2023-08-23 21:53:14 +0900

    [Gold I] Title: 방법을 출력하지 않는 숫자 맞추기, Time: 140 ms, Memory: 15976 KB -BaekjoonHub

---
+++ 백준/Gold/13392. 방법을 출력하지 않는 숫자 맞추기/방법을 출력하지 않는 숫자 맞추기.kt
@@ -0,0 +1,47 @@
+import kotlin.math.abs
+
+fun Int.spinPlusCnt(e: Int): Int {
+    return if (this <= e) e - this else 10 - this + e
+}
+
+fun Int.spinMinusCnt(e: Int): Int {
+    return if (this >= e) this - e else 10 + this - e
+}
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val N = readLine().toInt()
+    val start = readLine().reversed()
+    val end = readLine().reversed()
+
+    val dp = Array(N) { IntArray(10) }
+    val firstN = end[0] - '0'
+    repeat(10) {
+        dp[0][it] = (10 + it - firstN).coerceAtMost(abs(firstN - it))
+    }
+
+    for (i in 1 until N) {
+        val sb = start[i - 1] - '0'
+        val sn = start[i] - '0'
+        val e = end[i] - '0'
+
+        for (j in 0..9) {
+            val n = (sn + j) % 10
+            val plusCnt = n.spinPlusCnt(e)
+            val minusCnt = n.spinMinusCnt(e)
+
+//            println("$i $j $n $e $plusCnt $minusCnt")
+
+            dp[i][n] = (plusCnt + dp[i - 1][(plusCnt + sb + j) % 10]).coerceAtMost(minusCnt + dp[i - 1][(sb + j) % 10])
+        }
+    }
+
+//    repeat(N) { i ->
+//        repeat(10) { j ->
+//            print(dp[i][j])
+//            print(" ")
+//        }
+//        println()
+//    }
+
+    println(dp[N - 1][start[N - 1] - '0'])
+}
